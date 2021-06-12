e276ece487dee85c5688ba80f0d1ad9127c6c42f
Novitz Jean-Philippe <novitz@gmail.com>
2021-06-13 00:16:12 +0200

    content header and seauch input section with 3 jauges

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,6 @@ for (let i = 0; i < myCharts.length; i++) {
 
     test = document.createElement('canvas')
     test.setAttribute("id", "myChart"+i)
-console.log(test)
     item.append(test)
 
     var ctx = document.getElementById('myChart'+i).getContext('2d');
@@ -54,3 +53,84 @@ console.log(test)
         }
     });
 }
+
+
+var ctx2 = document.getElementById('mydoughnut').getContext('2d');
+var mydoughnut = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: [''],
+        datasets: [{
+            label: '# of Votes',
+            data: [78, 12],
+            backgroundColor: [
+                '#E6F2FF',
+                'white',
+            ],
+            borderColor: [
+                '#51A3FF',
+                'white'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins:{
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            x: {
+                display: false
+            },
+            y: {
+                display: false
+            }
+        },
+        responsive: false
+    }
+});
+
+var ctx3 = document.getElementById('mybars').getContext('2d');
+var mybars = new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: [' ',' ',' ',' ',' '],
+        datasets: [{
+            label: '',
+            data: [78, 10, 50, 30 ,90],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins:{
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            x: {
+                display: false
+            },
+            y: {
+                display: false
+            }
+        },
+        responsive: true
+    }
+});
