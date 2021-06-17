4b9cc3b10b5e67df959ea9c4ba0cbf8319275ca6
Novitz Jean-Philippe <novitz@gmail.com>
2021-06-17 23:52:58 +0200

    javascript for aniamate menu burger between small and big screens

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,30 @@
+/* ************************* */
+/* ***** LATERAL MENU ****** */
+/****************************/
+
+var menus = document.querySelectorAll("#menu, #sidebar-menu")
+var sidebar = document.getElementById("sidebar")
+var contentLogo = document.getElementById('content-logo')
+
+for (var menu of menus){
+    menu.addEventListener('click', ()=> {
+        if(sidebar.classList.contains('show')){
+            sidebar.classList.remove('show')
+            if(contentLogo.classList.contains('hide')){
+                contentLogo.classList.remove('hide')
+            }
+        } else {
+            sidebar.classList.add('show')
+            if(!contentLogo.classList.contains('hide')){
+                contentLogo.classList.add('hide')
+            }
+
+        }
+    })
+}
+
+//charts
+
 myCharts = document.getElementsByClassName('myChart')
 
 var j = 0
